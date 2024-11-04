35bdb5cf3ae67b3b36667bbda1b3911e97a0b97c
Amin Faruq <aminfaruk.fa@gmail.com>
2024-11-04 22:29:44 +0800

    feat: apply chucker

--- app/src/main/java/io/mindset/jagamental/di/NetworkModule.kt
+++ app/src/main/java/io/mindset/jagamental/di/NetworkModule.kt
@@ -1,7 +1,9 @@
 package io.mindset.jagamental.di
 
+import com.chuckerteam.chucker.api.ChuckerInterceptor
 import io.mindset.jagamental.data.remote.ApiService
 import io.mindset.jagamental.data.remote.MainRepository
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -12,12 +14,21 @@ const val apiUrl = "https://example.com/"
 
 val networkModule = module {
     single {
+        /*val authInterceptor = Interceptor { chain ->
+            val req = chain.request()
+            val requestHeaders = req.newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+            chain.proceed(requestHeaders)
+        }*/
         val loggingInterceptor =
             HttpLoggingInterceptor().apply {
                 level = HttpLoggingInterceptor.Level.BODY
             }
         OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .addInterceptor(ChuckerInterceptor(get()))
+            //.addInterceptor(authInterceptor)
             .build()
     }
 
