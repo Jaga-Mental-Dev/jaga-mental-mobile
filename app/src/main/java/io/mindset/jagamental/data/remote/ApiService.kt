34c48ec423a93cf209a245a4b609fa6e9fe88e00
Amin Faruq <aminfaruk.fa@gmail.com>
2024-11-05 18:07:23 +0800

    feat: implement auth api

--- app/src/main/java/io/mindset/jagamental/data/remote/ApiService.kt
+++ app/src/main/java/io/mindset/jagamental/data/remote/ApiService.kt
@@ -1,5 +1,37 @@
 package io.mindset.jagamental.data.remote
 
-interface ApiService {
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
 
+interface ApiService {
+    @POST("/auth/login")
+    @FormUrlEncoded
+    suspend fun doLogin(
+
+    )
+
+    @POST("/auth/register")
+    @FormUrlEncoded
+    suspend fun doRegister(
+
+    )
+
+    @POST("/oauth")
+    @FormUrlEncoded
+    suspend fun doOauth(
+
+    )
+
+    @PUT("/user/{id}")
+    @FormUrlEncoded
+    suspend fun doUpdateUser(
+
+    )
+
+    @GET("/user/me")
+    suspend fun doGetCurrentUser(
+
+    )
 }
