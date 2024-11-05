329e083a983598daa8fc68e8598d1e03cdf8c75e
gverse <galihputroaji.bs@gmail.com>
2024-11-05 11:35:58 +0700

    Feat: LoginScreen Unfinished

--- app/src/main/java/io/mindset/jagamental/ui/screen/login/LoginScreen.kt
+++ app/src/main/java/io/mindset/jagamental/ui/screen/login/LoginScreen.kt
@@ -1,2 +1,67 @@
 package io.mindset.jagamental.ui.screen.login
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.mindset.jagamental.R
+
+@Composable
+fun LoginScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .offset(x = 60.dp)
+                .size(width = 411.dp, height = 419.dp),
+            painter = painterResource(id = R.drawable.solar_people_nearby_bold_duotone),
+            contentDescription = null,
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 20.dp),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Image(
+                painter = painterResource(id = R.mipmap.ic_launcher),
+                contentDescription = null,
+            )
+            Text(
+                text = "Jaga Mental",
+                style = TextStyle(
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                )
+            )
+        }
+
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
