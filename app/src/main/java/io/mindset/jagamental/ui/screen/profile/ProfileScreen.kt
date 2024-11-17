a7a2eae9147930d9683eda377b303026702352e3
galihvsx <galihputroaji.bs@gmail.com>
2024-11-17 14:27:27 +0700

    feat: Implement profile screen UI (ProfilePage)
    This commit implements the user interface for the profile
    screen. It includes displaying user information, account
    settings, and a logout option.  It also utilizes Coil
    for image loading and incorporates UI elements like Cards,
    Rows, and Icons for a visually appealing layout.  The
    dynamic color option in the theme is disabled for
    consistency.

--- app/src/main/java/io/mindset/jagamental/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/io/mindset/jagamental/ui/screen/profile/ProfileScreen.kt
@@ -1,23 +1,44 @@
 package io.mindset.jagamental.ui.screen.profile
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ChevronRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.google.firebase.auth.FirebaseAuth
+import coil3.compose.AsyncImage
+import io.mindset.jagamental.R
 import io.mindset.jagamental.navigation.Screen
+import io.mindset.jagamental.ui.theme.tertiaryContainerLightHighContrast
 import io.mindset.jagamental.utils.StatusBarColorHelper
 import org.koin.androidx.compose.koinViewModel
 
@@ -25,7 +46,6 @@ import org.koin.androidx.compose.koinViewModel
 fun ProfileScreen(navController: NavController, paddingValues: PaddingValues) {
 
     val viewModel: ProfileViewModel = koinViewModel()
-    val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     StatusBarColorHelper(Color.White, useDarkIcon = true)
     Column(
@@ -33,28 +53,212 @@ fun ProfileScreen(navController: NavController, paddingValues: PaddingValues) {
             .fillMaxSize()
             .padding(paddingValues)
             .background(color = Color.White),
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .clip(RoundedCornerShape(16.dp)),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.LightGray.copy(alpha = 0.3f),
+            )
+        ) {
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(CircleShape)
+                            .border(1.dp, Color.LightGray, CircleShape)
+                    ) {
+                        AsyncImage(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            model = "https://i.pravatar.cc/300?img=53",
+                            contentDescription = "Profile Image",
+                            contentScale = ContentScale.Crop,
+                            placeholder = painterResource(id = R.drawable.solar__face_scan_circle_bold),
+                        )
+                    }
+                    Row(
+                        modifier = Modifier.padding(start = 16.dp),
+                    ) {
+                        HeaderItem(
+                            modifier = Modifier.weight(1f),
+                            title = "12",
+                            value = "Journal"
+                        )
+
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        HeaderItem(
+                            modifier = Modifier.weight(1f),
+                            title = "Verified",
+                            value = "Status"
+                        )
+                    }
+                }
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.LightGray.copy(alpha = 0.3f))
+        ) {
             Text(
-                text = "Profile!",
+                text = "Account",
                 style = TextStyle(
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 32.sp
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+            HorizontalDivider(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                color = Color.LightGray
+            )
+            ProfileListItem(
+                modifier = Modifier.padding(4.dp),
+                label = "Edit Profile"
+            )
+            ProfileListItem(
+                modifier = Modifier.padding(4.dp),
+                label = "Change Password"
+            )
+
+            Text(
+                text = "General",
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+            HorizontalDivider(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                color = Color.LightGray
+            )
+            ProfileListItem(
+                modifier = Modifier.padding(4.dp),
+                label = "Rate App"
+            )
+            ProfileListItem(
+                modifier = Modifier.padding(4.dp),
+                label = "Feedback"
+            )
+            ProfileListItem(
+                modifier = Modifier.padding(4.dp),
+                label = "Privacy Policy"
+            )
+            ProfileListItem(
+                modifier = Modifier.padding(4.dp),
+                label = "Terms of Service"
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.LightGray.copy(alpha = 0.3f))
+                .clickable(onClick = {
+                    viewModel.logout()
+                    navController.navigate(Screen.Auth) {
+                        popUpTo(Screen.Auth.Login) {
+                            inclusive = true
+                        }
+                    }
+                }),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = "Logout",
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Red
+            )
+
+            Icon(
+                imageVector = Icons.Rounded.ChevronRight,
+                contentDescription = "Profile",
+                tint = Color.Red,
+                modifier = Modifier
+                    .padding(end = 16.dp)
+                    .size(20.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun HeaderItem(
+    modifier: Modifier,
+    title: String,
+    value: String
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontSize = 16.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            modifier = Modifier,
+            text = value,
+            fontSize = 12.sp
+        )
+    }
+}
+
+@Composable
+fun ProfileListItem(
+    modifier: Modifier = Modifier,
+    label: String
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp, vertical = 4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = label,
+                style = TextStyle(
+                    color = Color.Black,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
                 )
             )
 
-            Button(
-                onClick = {
-                    viewModel.logout()
-                    navController.navigate(Screen.Auth.Login) {
-                        popUpTo(Screen.App.Dashboard) { inclusive = true }
-                    }
-                }
-            ) {
-                Text("Logout")
-            }
+            Icon(
+                imageVector = Icons.Rounded.ChevronRight,
+                contentDescription = "Profile",
+                tint = tertiaryContainerLightHighContrast,
+                modifier = Modifier.size(20.dp)
+            )
         }
     }
 }
--- app/src/main/java/io/mindset/jagamental/ui/theme/Theme.kt
+++ app/src/main/java/io/mindset/jagamental/ui/theme/Theme.kt
@@ -73,7 +73,7 @@ private val highContrastLightColorScheme = lightColorScheme(
 fun JagaMentalTheme(
     darkTheme: Boolean = false /*isSystemInDarkTheme()*/,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
