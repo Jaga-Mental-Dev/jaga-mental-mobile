6841e5c90fbbd03970a85ed44415234ec0ca2f89
galih <galihputroaji.bs@gmail.com>
2024-11-24 23:05:58 +0700

    feat: Implement remote config fetching (RefProfile)
    Implement fetching and activation of remote config.  Logs
    whether the config was updated or not.

--- app/src/main/java/io/mindset/jagamental/ui/MainActivity.kt
+++ app/src/main/java/io/mindset/jagamental/ui/MainActivity.kt
@@ -1,13 +1,19 @@
 package io.mindset.jagamental.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.lifecycleScope
 import io.mindset.jagamental.ui.screen.root.RootScreen
 import io.mindset.jagamental.ui.theme.JagaMentalTheme
+import io.mindset.jagamental.utils.RemoteConfigHelper
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    val remoteConfig = RemoteConfigHelper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +23,17 @@ class MainActivity : ComponentActivity() {
                 RootScreen()
             }
         }
+
+        lifecycleScope.launch {
+            val remoteConfigHelper = RemoteConfigHelper()
+            val isUpdated = remoteConfigHelper.fetchAndActivate()
+            if (isUpdated) {
+                Log.d("RemoteConfig", "Remote Config Updated")
+            } else {
+                Log.d("RemoteConfig", "Remote Config Not Updated")
+            }
+        }
+
     }
+
 }
