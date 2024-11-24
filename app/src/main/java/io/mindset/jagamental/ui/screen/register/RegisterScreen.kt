7f67a5651f7a891196b296c5f1eca6dfcbff27c6
galih <galihputroaji.bs@gmail.com>
2024-11-24 23:09:09 +0700

    feat: Implement Google Sign-In for registration (RefProfile)
    - Added Google Oauth button to the registration screen.
    - Implemented Google Oauth functionality.
    - Added loading state during sign-in process.
    - Updated the registerWithEmailPassword function to include name.
    - Minor UI updates and string resource adjustments.

--- app/src/main/java/io/mindset/jagamental/ui/screen/register/RegisterScreen.kt
+++ app/src/main/java/io/mindset/jagamental/ui/screen/register/RegisterScreen.kt
@@ -2,6 +2,8 @@ package io.mindset.jagamental.ui.screen.register
 
 import android.content.res.Configuration
 import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -42,8 +44,9 @@ import androidx.navigation.NavHostController
 import io.mindset.jagamental.R
 import io.mindset.jagamental.data.model.AuthState
 import io.mindset.jagamental.navigation.Screen
+import io.mindset.jagamental.ui.component.auth.AuthLoader
 import io.mindset.jagamental.ui.component.auth.FilledButton
-import io.mindset.jagamental.ui.component.auth.OAuthButton
+import io.mindset.jagamental.ui.component.auth.GoogleSignInButton
 import io.mindset.jagamental.ui.component.auth.RoundedTextField
 import io.mindset.jagamental.ui.component.auth.TextDivider
 import io.mindset.jagamental.ui.screen.login.isValidEmail
@@ -66,189 +69,215 @@ fun RegisterScreen(navController: NavHostController) {
 
     val viewModel: RegisterViewModel = koinViewModel()
     val uiState = viewModel.uiState.collectAsState()
+    val isLoading = viewModel.isLoading.collectAsState()
+
+    val oauthLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        viewModel.handleOauthResult(result)
+    }
+
+    val onGoogleSignInClick = {
+        val signInIntent = viewModel.oauthIntent()
+        oauthLauncher.launch(signInIntent)
+    }
 
     StatusBarColorHelper()
     Scaffold { padding ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(padding)
-                .background(Color.White)
-        ) {
-            val configuration = LocalConfiguration.current
-            val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-            val screenWidth = configuration.screenWidthDp.dp
-
-            Image(
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .fillMaxHeight(if (!isLandscape) 0.8f else 0.8f)
-                    .fillMaxWidth(if (!isLandscape) 0.8f else 0.8f)
-                    .let { modifier ->
-                        if (isLandscape) {
-                            modifier.offset(x = screenWidth * 0.28f)
-                        } else {
-                            modifier
-                        }
-                    },
-                painter = painterResource(id = R.drawable.solar_people_nearby_bold_duotone),
-                contentDescription = null,
-            )
-
-            Column(
+        if (isLoading.value) {
+            val loadingText = stringResource(R.string.signing_up)
+            AuthLoader(loadingText)
+        } else {
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(horizontal = 20.dp)
-                    .verticalScroll(scrollState),
-                verticalArrangement = Arrangement.Center,
+                    .padding(padding)
+                    .background(Color.White)
             ) {
+                val configuration = LocalConfiguration.current
+                val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+                val screenWidth = configuration.screenWidthDp.dp
+
                 Image(
-                    painter = painterResource(id = R.mipmap.ic_launcher),
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .fillMaxHeight(if (!isLandscape) 0.8f else 0.8f)
+                        .fillMaxWidth(if (!isLandscape) 0.8f else 0.8f)
+                        .let { modifier ->
+                            if (isLandscape) {
+                                modifier.offset(x = screenWidth * 0.28f)
+                            } else {
+                                modifier
+                            }
+                        },
+                    painter = painterResource(id = R.drawable.solar_people_nearby_bold_duotone),
                     contentDescription = null,
                 )
 
-                Text(
-                    modifier = Modifier.padding(top = 32.dp),
-                    text = stringResource(id = R.string.app_name),
-                    style = TextStyle(
-                        fontSize = 32.sp,
-                        fontWeight = FontWeight(600),
-                        color = MaterialTheme.colorScheme.onBackground,
-                        textAlign = TextAlign.Center,
-                    )
-                )
-
-                Text(
-                    modifier = Modifier.padding(top = 16.dp),
-                    text = stringResource(id = R.string.login_subtitle),
-                    style = TextStyle(
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight(400),
-                        color = MaterialTheme.colorScheme.secondary,
-                        textAlign = TextAlign.Start,
-                        lineHeight = 24.sp,
-                    )
-                )
-
                 Column(
-                    modifier = Modifier.padding(top = 40.dp),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 20.dp)
+                        .verticalScroll(scrollState),
+                    verticalArrangement = Arrangement.Center,
                 ) {
-                    RoundedTextField(
-                        modifier = Modifier,
-                        value = fullName.value,
-                        onValueChange = { fullName.value = it },
-                        label = "Full Name",
-                        placeholder = "John Doe",
+                    Image(
+                        painter = painterResource(id = R.mipmap.ic_launcher),
+                        contentDescription = null,
                     )
 
-                    RoundedTextField(
-                        modifier = Modifier.padding(top = 16.dp),
-                        value = email.value,
-                        onValueChange = {
-                            email.value = it
-                            isEmailValid.value = isValidEmail(it)
-                            emailError.value = if (isEmailValid.value) "" else "Invalid email address"
-                        },
-                        label = stringResource(id = R.string.login_email_label),
-                        placeholder = stringResource(id = R.string.login_email_placeholder),
-                    )
-                    if (emailError.value.isNotEmpty()) {
-                        Text(
-                            modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp),
-                            text = emailError.value,
-                            color = Color.Red,
-                            style = TextStyle(fontSize = 12.sp)
-                        )
-                    }
-
-                    RoundedTextField(
-                        modifier = Modifier.padding(top = 16.dp),
-                        value = password.value,
-                        onValueChange = {
-                            password.value = it
-                            isPasswordValid.value = isValidPassword(it)
-                            passwordError.value = if (isPasswordValid.value) "" else "Password must be at least 6 characters"
-                        },
-                        label = stringResource(id = R.string.login_password_label),
-                        placeholder = "",
-                        type = "password",
-                        imeAction = ImeAction.Done
-                    )
-                    if (passwordError.value.isNotEmpty()) {
-                        Text(
-                            modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp),
-                            text = passwordError.value,
-                            color = Color.Red,
-                            style = TextStyle(fontSize = 12.sp)
-                        )
-                    }
-
-                    if (registerError.value.isNotEmpty()) {
-                        Text(
-                            modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp),
-                            text = registerError.value,
-                            color = Color.Red,
-                            style = TextStyle(fontSize = 12.sp)
-                        )
-                    }
-
-                    FilledButton(
-                        modifier = Modifier
-                            .padding(top = 24.dp)
-                            .height(48.dp),
-                        name = "Daftar Sekarang",
-                        onClick = {
-                            viewModel.registerWithEmailPassword(email.value, password.value)
-                        },
-                        enabled = isEmailValid.value && isPasswordValid.value
-                    )
-
-                    TextDivider(
-                        modifier = Modifier.padding(top = 20.dp, bottom = 20.dp),
-                        "Atau Masuk Dengan"
-                    )
-
-                    OAuthButton(
-                        modifier = Modifier
-                            .align(Alignment.CenterHorizontally),
-                        onclick = {},
-                    )
-                }
-            }
-            Box(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .background(Color.White)
-                    .padding(bottom = 10.dp),
-            ) {
-                Row(
-                    Modifier
-                        .height(40.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
                     Text(
-                        text = stringResource(R.string.have_account),
+                        modifier = Modifier.padding(top = 32.dp),
+                        text = stringResource(id = R.string.app_name),
                         style = TextStyle(
-                            fontWeight = FontWeight.Normal
+                            fontSize = 32.sp,
+                            fontWeight = FontWeight(600),
+                            color = MaterialTheme.colorScheme.onBackground,
+                            textAlign = TextAlign.Center,
                         )
                     )
-                    TextButton(
-                        onClick = {
-                            navController.navigate(Screen.Auth.Login) {
-                                popUpTo(Screen.Auth.Register) { inclusive = true }
+
+                    Text(
+                        modifier = Modifier.padding(top = 16.dp),
+                        text = stringResource(id = R.string.login_subtitle),
+                        style = TextStyle(
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight(400),
+                            color = MaterialTheme.colorScheme.secondary,
+                            textAlign = TextAlign.Start,
+                            lineHeight = 24.sp,
+                        )
+                    )
+
+                    Column(
+                        modifier = Modifier.padding(top = 40.dp),
+                    ) {
+                        RoundedTextField(
+                            modifier = Modifier,
+                            value = fullName.value,
+                            onValueChange = { fullName.value = it },
+                            label = "Full Name",
+                            placeholder = "John Doe",
+                        )
+
+                        RoundedTextField(
+                            modifier = Modifier.padding(top = 16.dp),
+                            value = email.value,
+                            onValueChange = {
+                                email.value = it
+                                isEmailValid.value = isValidEmail(it)
+                                emailError.value =
+                                    if (isEmailValid.value) "" else "Invalid email address"
+                            },
+                            label = stringResource(id = R.string.login_email_label),
+                            placeholder = stringResource(id = R.string.login_email_placeholder),
+                        )
+                        if (emailError.value.isNotEmpty()) {
+                            Text(
+                                modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp),
+                                text = emailError.value,
+                                color = Color.Red,
+                                style = TextStyle(fontSize = 12.sp)
+                            )
+                        }
+
+                        RoundedTextField(
+                            modifier = Modifier.padding(top = 16.dp),
+                            value = password.value,
+                            onValueChange = {
+                                password.value = it
+                                isPasswordValid.value = isValidPassword(it)
+                                passwordError.value =
+                                    if (isPasswordValid.value) "" else "Password must be at least 6 characters"
+                            },
+                            label = stringResource(id = R.string.login_password_label),
+                            placeholder = "",
+                            type = "password",
+                            imeAction = ImeAction.Done
+                        )
+                        if (passwordError.value.isNotEmpty()) {
+                            Text(
+                                modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp),
+                                text = passwordError.value,
+                                color = Color.Red,
+                                style = TextStyle(fontSize = 12.sp)
+                            )
+                        }
+
+                        if (registerError.value.isNotEmpty()) {
+                            Text(
+                                modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp),
+                                text = registerError.value,
+                                color = Color.Red,
+                                style = TextStyle(fontSize = 12.sp)
+                            )
+                        }
+
+                        FilledButton(
+                            modifier = Modifier
+                                .padding(top = 24.dp)
+                                .height(48.dp),
+                            name = stringResource(R.string.create_account),
+                            onClick = {
+                                viewModel.registerWithEmailPassword(
+                                    fullName.value,
+                                    email.value,
+                                    password.value
+                                )
+                            },
+                            enabled = isEmailValid.value && isPasswordValid.value
+                        )
+
+                        TextDivider(
+                            modifier = Modifier.padding(top = 20.dp, bottom = 20.dp),
+                            stringResource(R.string.or_label)
+                        )
+
+                        GoogleSignInButton(
+                            modifier = Modifier
+                                .height(48.dp)
+                                .fillMaxWidth(),
+                            onClick = {
+                                onGoogleSignInClick()
                             }
-                        },
-                        colors = ButtonDefaults.textButtonColors(contentColor = tertiaryContainerLightHighContrast),
-                        modifier = Modifier.offset(x = (-8).dp)
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .background(Color.White)
+                        .padding(bottom = 10.dp),
+                ) {
+                    Row(
+                        Modifier
+                            .height(40.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
                         Text(
-                            text = stringResource(id = R.string.login),
+                            text = stringResource(R.string.have_account),
                             style = TextStyle(
-                                fontWeight = FontWeight.SemiBold
+                                fontWeight = FontWeight.Normal
                             )
                         )
+                        TextButton(
+                            onClick = {
+                                navController.navigate(Screen.Auth.Login) {
+                                    popUpTo(Screen.Auth.Register) { inclusive = true }
+                                }
+                            },
+                            colors = ButtonDefaults.textButtonColors(contentColor = tertiaryContainerLightHighContrast),
+                            modifier = Modifier.offset(x = (-8).dp)
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.login),
+                                style = TextStyle(
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            )
+                        }
                     }
                 }
             }
