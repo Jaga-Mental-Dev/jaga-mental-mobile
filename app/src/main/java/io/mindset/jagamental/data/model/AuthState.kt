888b6254e616f8361339b2a1eddfef3971ba9760
galih <galihputroaji.bs@gmail.com>
2024-11-26 21:06:09 +0700

    feat: remove AuthState model (main)
    Removed unnecessary AuthState model.

--- app/src/main/java/io/mindset/jagamental/data/model/AuthState.kt
+++
@@ -1,9 +0,0 @@
-package io.mindset.jagamental.data.model
-
-import com.google.firebase.auth.FirebaseUser
-
-sealed class AuthState {
-    data object Idle : AuthState()
-    data class Success(val user: FirebaseUser?) : AuthState()
-    data class Error(val message: String) : AuthState()
-}
