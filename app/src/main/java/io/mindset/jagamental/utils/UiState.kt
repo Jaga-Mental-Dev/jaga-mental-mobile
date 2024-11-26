d593731d5f1ddd1dfe428047c74681e36183182b
galih <galihputroaji.bs@gmail.com>
2024-11-26 21:07:14 +0700

    feat: Improve UiState handling (journal-flow)
    Improved the UiState class by:
    
    -   Renaming the `ignoredError` field in the `Error`
        state to `message` for better clarity.
    -   Adding an `Idle` state to represent an
        inactive state.

--- app/src/main/java/io/mindset/jagamental/utils/UiState.kt
+++ app/src/main/java/io/mindset/jagamental/utils/UiState.kt
@@ -3,6 +3,7 @@ package io.mindset.jagamental.utils
 sealed class UiState<out T: Any?> {
 
     data class Success<out T>(val data: T) : UiState<T>()
-    data class Error(val ignoredError: String) : UiState<Nothing>()
+    data class Error(val message: String) : UiState<Nothing>()
     data object Loading : UiState<Nothing>()
+    data object Idle : UiState<Nothing>()
 }
