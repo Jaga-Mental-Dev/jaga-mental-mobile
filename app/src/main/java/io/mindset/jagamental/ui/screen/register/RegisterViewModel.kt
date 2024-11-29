e2d938fffd1625a18e8039efabc6173dd40a38db
galih <galihputroaji.bs@gmail.com>
2024-11-29 14:42:37 +0700

    feat: Remove unnecessary LoginRepository dependency (journal-flow)
    Refactor RegisterViewModel to remove the unnecessary
    dependency on LoginRepository.  Updated log messages
    for clarity.

--- app/src/main/java/io/mindset/jagamental/ui/screen/register/RegisterViewModel.kt
+++ app/src/main/java/io/mindset/jagamental/ui/screen/register/RegisterViewModel.kt
@@ -7,22 +7,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseUser
 import io.mindset.jagamental.data.domain.AuthRepository
-import io.mindset.jagamental.data.domain.LoginRepository
 import io.mindset.jagamental.utils.UiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class RegisterViewModel(
-    private val authRepository: AuthRepository,
-    private val loginRepository: LoginRepository
+    private val authRepository: AuthRepository
 ) : ViewModel() {
 
     private val _registerState = MutableStateFlow<UiState<FirebaseUser?>>(UiState.Idle)
     val registerState = _registerState.asStateFlow()
 
     fun oauthIntent(): Intent = authRepository.signInIntent()
-
 
     fun registerWithEmailPassword(name: String, email: String, password: String) {
         viewModelScope.launch {
@@ -36,11 +33,14 @@ class RegisterViewModel(
 
                     is UiState.Error -> {
                         _registerState.value = UiState.Error(authState.message)
-                        Log.d("LoginViewModel", "signInWithEmailPassword: ${authState.message}")
+                        Log.d(
+                            "RegisterViewModel",
+                            "registerWithEmailPassword: ${authState.message}"
+                        )
                     }
 
                     else -> {
-                        Log.d("LoginViewModel", "signInWithEmailPassword: $authState")
+                        Log.d("RegisterViewModel", "registerWithEmailPassword: $authState")
                     }
                 }
             }
