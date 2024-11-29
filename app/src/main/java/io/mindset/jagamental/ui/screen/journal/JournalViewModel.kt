f86176a5e94c5b123e99c2291906ba228c6c1bc0
galih <galihputroaji.bs@gmail.com>
2024-11-29 13:33:50 +0700

    feat: Implement journal screen with data fetching (journal-flow)
    This commit implements the journal screen, fetching
    journal entries from a repository based on the
    selected date.  It handles loading and error states,
    displays a progress indicator, and shows an empty
    state message if no journals are found for the date.
    Navigation to the journal result screen is also
    implemented.

--- app/src/main/java/io/mindset/jagamental/ui/screen/journal/JournalViewModel.kt
+++ app/src/main/java/io/mindset/jagamental/ui/screen/journal/JournalViewModel.kt
@@ -1,4 +1,40 @@
 package io.mindset.jagamental.ui.screen.journal
 
-class JournalViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.mindset.jagamental.data.domain.JournalRepository
+import io.mindset.jagamental.data.model.response.JournalData
+import io.mindset.jagamental.utils.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class JournalViewModel(
+    private val journalRepository: JournalRepository
+) : ViewModel() {
+    private val _journalState = MutableStateFlow<UiState<List<JournalData?>?>>(UiState.Idle)
+    val journalState = _journalState.asStateFlow()
+
+    init {
+        var currentDate = LocalDate.now()
+        val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        getJournalsByDate(currentDate.format(dateFormatter))
+    }
+
+    fun getJournalsByDate(date: String) {
+        viewModelScope.launch {
+            _journalState.value = UiState.Loading
+            journalRepository.getJournalByDate(date).collect { response ->
+                Log.d("JournalViewModel", "getJournalsByDate: $response")
+                _journalState.value = response
+            }
+        }
+    }
+
+    fun resetState() {
+        _journalState.value = UiState.Loading
+    }
 }
