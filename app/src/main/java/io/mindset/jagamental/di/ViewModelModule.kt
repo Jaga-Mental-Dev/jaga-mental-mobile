55d4fcb6e9d92b36bce2ab02ff2f9c0613f3eba5
galih <galihputroaji.bs@gmail.com>
2024-11-29 14:40:24 +0700

    feat: Add Journal ViewModels (journal-flow)
    Add ViewModels for Journal feature:
    
    - JournalViewModel
    - InputJournalViewModel
    - JournalResultViewModel
    
    Update dependencies for ViewModels.  Update MainViewModel dependency.

--- app/src/main/java/io/mindset/jagamental/di/ViewModelModule.kt
+++ app/src/main/java/io/mindset/jagamental/di/ViewModelModule.kt
@@ -2,9 +2,12 @@ package io.mindset.jagamental.di
 
 import io.mindset.jagamental.ui.MainViewModel
 import io.mindset.jagamental.ui.screen.dashboard.DashboardViewModel
+import io.mindset.jagamental.ui.screen.journal.JournalViewModel
 import io.mindset.jagamental.ui.screen.journal.add.capture.CaptureViewModel
+import io.mindset.jagamental.ui.screen.journal.add.input.InputJournalViewModel
 import io.mindset.jagamental.ui.screen.journal.add.photoresult.PhotoResultViewModel
 import io.mindset.jagamental.ui.screen.journal.add.preview.ResultPreviewViewModel
+import io.mindset.jagamental.ui.screen.journal.result.JournalResultViewModel
 import io.mindset.jagamental.ui.screen.login.LoginViewModel
 import io.mindset.jagamental.ui.screen.profile.ProfileViewModel
 import io.mindset.jagamental.ui.screen.register.RegisterViewModel
@@ -13,13 +16,16 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { MainViewModel(get()) }
+    viewModel { MainViewModel(get(), get()) }
     viewModel { LoginViewModel(get(), get()) }
     viewModel { DashboardViewModel(get()) }
-    viewModel { RegisterViewModel(get(), get()) }
+    viewModel { RegisterViewModel(get()) }
     viewModel { RootViewModel(get()) }
     viewModel { ProfileViewModel(get()) }
     viewModel { CaptureViewModel() }
-    viewModel { PhotoResultViewModel() }
+    viewModel { PhotoResultViewModel(get(), get()) }
     viewModel { ResultPreviewViewModel(get()) }
+    viewModel { JournalViewModel(get()) }
+    viewModel { InputJournalViewModel(get(), get()) }
+    viewModel { JournalResultViewModel(get(), get()) }
 }
