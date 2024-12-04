3266ca2500db51728045045f81e0fd215d6d5163
galih <galihputroaji.bs@gmail.com>
2024-12-04 13:08:58 +0700

    Fix: Update UiState class type parameter
    Improve type safety of UiState class by using
    out keyword for type parameter T.
    
    (chat-bot)

--- app/src/main/java/io/mindset/jagamental/utils/UiState.kt
+++ app/src/main/java/io/mindset/jagamental/utils/UiState.kt
@@ -1,6 +1,6 @@
 package io.mindset.jagamental.utils
 
-sealed class UiState<out T: Any?> {
+sealed class UiState<out T : Any?> {
 
     data class Success<out T>(val data: T) : UiState<T>()
     data class Error(val message: String) : UiState<Nothing>()
