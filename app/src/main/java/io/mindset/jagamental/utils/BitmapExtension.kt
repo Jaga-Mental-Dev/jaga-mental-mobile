5fe12b32e28e0a97bce7890e714e184b61fbe010
galih <galihputroaji.bs@gmail.com>
2024-12-04 12:35:26 +0700

    Fix: Correct image rotation for front camera (chat-bot)
    Improved the `rotateBitmap` function to handle
    image rotation correctly for both front and
    rear cameras.  The function now takes an
    additional boolean parameter to specify the
    camera used and adjusts the scaling
    accordingly.

--- app/src/main/java/io/mindset/jagamental/utils/BitmapExtension.kt
+++ app/src/main/java/io/mindset/jagamental/utils/BitmapExtension.kt
@@ -3,10 +3,12 @@ package io.mindset.jagamental.utils
 import android.graphics.Bitmap
 import android.graphics.Matrix
 
-fun Bitmap.rotateBitmap(rotationDegrees: Int): Bitmap {
+fun Bitmap.rotateBitmap(rotationDegrees: Int, isFrontCamera: Boolean = true): Bitmap {
     val matrix = Matrix().apply {
-        postRotate(-rotationDegrees.toFloat())
-        postScale(-1f, -1f)
+        postRotate(rotationDegrees.toFloat())
+        if (isFrontCamera) {
+            postScale(-1f, 1f)
+        }
     }
 
     return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
